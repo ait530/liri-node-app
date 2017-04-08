0e25fa046c057c366c31d44cd1ddd0636bdb7072
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-04-08 09:08:09 -0500

    cleaned code up

--- liri.js
+++ liri.js
@@ -1,14 +1,16 @@
-//////////////////////////////////////////////////////////////////////////// 
-// Keys & NPM REQUIRES
-////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+// Keys, NPM REQUIRES, Global Variables
+/////////////////////////////////////////////////////////////////////////////
 // require - provides a way to load a module (keys.js), needs "./" since keys.js is in the same (current) directory as liri.js 
 // accesses all of the exports in the key.js file. This can be confirmed when "node liri.js" is run in the command line since the console.log from the keys.js file will output 'this is loaded.'
 var keys = require('./keys.js');
 
 // process.argv will print out any command line arguments
 var nodeArgs = process.argv;
-// outputs the command line arguments
-console.log(nodeArgs);
+// // outputs the command line arguments
+// console.log(nodeArgs);
 
 ////////////////////////////////
 // *Don't forget to npm intall:/
@@ -23,15 +25,16 @@ var twitter = require('twitter');
 var spotify = require('spotify');
 // Loads Request module, Here we incorporate the "request" npm package
 var request = require('request');
-
+// Loads fs
+var fs = require('fs');
 
 // Global Scope Variables
-var songName = '';
+var songName = process.argv[3];
 var movieName = process.argv[3];
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
-
-
-////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 // TWITTER REQUEST
 /////////////////////////////////////////////////////////////////////////////
 // if the 2nd argument is 'my-tweets'...
@@ -51,13 +54,11 @@ if (process.argv[2] === 'my-tweets') {
       // If no error, 
       if (!error) {  
       
-
         // Repeat for up to 20 tweets...
         for (i = 0; i < 20; i++) {
         // log tweets.
         console.log(tweets);
-        
-
+    
         }
       }
     })
@@ -65,49 +66,37 @@ if (process.argv[2] === 'my-tweets') {
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
 /////////////////////////////////////////////////////////////////////////////
 // SPOTIFY REQUEST
 /////////////////////////////////////////////////////////////////////////////
-// if (process.argv[2] === 'spotify-this-song' && process.argv[3] === songName) {
+if (process.argv[2] === 'spotify-this-song' && process.argv[3] === songName) {
 
-//   spotifyRequest();
+  spotifyRequest();
 
-//   function spotifyRequest() {
+  function spotifyRequest() {
     
 
-//     // spotify URL + string from array + the api key
-//     var queryURL = 'https://api.spotify.com' + songName + '/v1/albums/{id}/tracks';
+    // spotify URL + string from array + the api key
+    var queryURL = 'https://api.spotify.com' + songName + '/v1/albums/{id}/tracks';
     
-//       // Creating an AJAX (asynchronous HTTP request) calls the spotify API for the song name.
-//       $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//       }).done(function(response) {
-//         console.log(JSON.stringify(tweets, null ,2));
+      // Creating an AJAX (asynchronous HTTP request) calls the spotify API for the song name.
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+        console.log(response);
       
-//       })
-//   }
+      })
+  }
 
 
-// }
+}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// // Client ID
-//   var client_id = "e25f7a6dfece48839bff27e5dac13849";
-
-//   // Client Secret
-//   var client_secret = "fe74b2d270684b84a298f724fac8f68e";
-
-
-
-
-///////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 // OMDB REQUEST
-///////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 // if the 2nd argument is 'my-tweets'...
 if (process.argv[2] === 'movie-this' && process.argv[3] === movieName) {
 
@@ -149,90 +138,25 @@ if (process.argv[2] === 'movie-this' && process.argv[3] === movieName) {
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
 /////////////////////////////////////////////////////////////////////////////
 // READING .TEXTFILE
 /////////////////////////////////////////////////////////////////////////////
-// fs is an NPM package for reading and writing files
-// var fs = require("fs");
+var textFile = process.argv[2];
 
-// var textFile = process.argv[2];
+// This block of code will read from the "movies.txt" file.
+// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+// The code will store the contents of the reading inside the variable "data"
+fs.readFile("random.txt", "utf8", function(error, data){
 
-// // This block of code will read from the "movies.txt" file.
-// // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-// // The code will store the contents of the reading inside the variable "data"
-// fs.readFile("movies.txt", "utf8", function(error, data) {
+  // We will then print the contents of data
+  console.log(data);
 
-//   // We will then print the contents of data
-//   console.log(data);
+  // Then split it by commas (to make it more readable)
+  var dataArr = data.split(",");
 
-//   // Then split it by commas (to make it more readable)
-//   var dataArr = data.split(",");
+  // We will then re-display the content as an array for later use.
+  console.log(dataArr);
 
-//   // We will then re-display the content as an array for later use.
-//   console.log(dataArr);
+  });
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// });
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////
-// NOTES
-/////////////////////////////////////////////////////////////////////////////
-// e.g.) commandline 
-// input: node liri.js my-tweets  
-// output: shows your last 20 tweets and when they were created at in your terminal/bash window.
-
-// input: 
-// node liri.js spotify-this-song '<song name here>'
-// output: This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-
-// if no song is provided then your program will default to
-// The Sign" by Ace of Base
-
-
-//input:
-// node liri.js movie-this '<movie name here>'
-// output:
-//   * Title of the movie.
-   // * Year the movie came out.
-   // * IMDB Rating of the movie.
-   // * Country where the movie was produced.
-   // * Language of the movie.
-   // * Plot of the movie.
-   // * Actors in the movie.
-   // * Rotten Tomatoes Rating.
-   // * Rotten Tomatoes URL.
-
-// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-// It's on Netflix!
-
-
-// input:
-// node liri.js do-what-it-says
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-// Feel free to change the text in that document to test out the feature for other commands.
-
-
-
-
-
-
-
-// Might be useful...
-// parsefloat() for decimal numbers
-// 'npm init' for package.json files 
-// 'npm install *whatever* --save'
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
