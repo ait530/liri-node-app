4d8dd9ba8af1be55f63e57b0f9cba4bfedbae0cd
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-04-09 19:07:32 -0500

    updated twitter functionality

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-// console.log('key.js is loaded');
-
 // export -- an object which is shared between all instances of the current module and made accessible through require().
 exports.twitterKeys = {
   consumer_key: 'IwhhnyzWiCwg1jZUTKq2ccWUH',
@@ -7,3 +5,6 @@ exports.twitterKeys = {
   access_token_key: '847112460951801856-j6w3B5EMrDXbKEq0mP08I7pmQL1J8hJ',
   access_token_secret: 'bxxGGV3x1ibAF7wvxjUpyIwYLWmm7ifAhrlEYtCg6OJfH',
 }
+
+// This will log to the command line when liri.js accessess the exports
+// console.log('key.js is loaded');
